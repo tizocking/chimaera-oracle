566ff3e4ba2b5218a2d593ad644f4deb6e96cf48
Douglas <douglasking29@gmail.com>
2014-03-11 11:58:42 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChimaeraOracle::Application.config.secret_token = '0cdac97de5ef29a7f87b1386f8899f6fbbeff9521991498f085319878dfaa3245a2f1da3621a29e732782c73043d65d3315b0bbb15a2f305f689fb4239e62831'
